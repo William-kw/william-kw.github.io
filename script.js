2804882a7cf3e23d03720ce696eb580340dff6d5
William-kw <williamkenfack4@gmail.com>
2024-03-02 13:43:33 +0100

    le 02 Mars

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const toggleLinksMenu = document.querySelector(".nav .toggle-links"),
   navLinkElement = document.querySelector(".nav .links"),
-  toggleContactRightBarMenu = document.querySelector(
-    ".nav .toggle-contact-right-bar"
-  ),
+  toggleContactRightBarMenu = document.querySelector(".nav .toggle-contact-right-bar"),
   navLinks = document.querySelectorAll(".nav .links .nav-link"),
   contactRightBar = document.querySelector(".contact-right-bar"),
   closeContact = document.querySelector(".contact-right-bar .close-contact"),
   contactOverlay = document.querySelector(".contact-overlay"),
-  loading = document.querySelector(".loading");
+  loading = document.querySelector(".loading"),
+  content = document.querySelector(".content"),
+  scrollIndicator = document.querySelector(".scroll");
+
+// scroll incator
+const obsever = new IntersectionObserver(handleIntersect)
+obsever.observe(content)
+function handleIntersect(entries) {
+  const el = entries[0]
+  if (el.isIntersecting) {
+    window.addEventListener("scroll", indicatorAnimation)
+  } else {
+    window.removeEventListener("scroll", indicatorAnimation)
+  }
+}
+function indicatorAnimation() {
+  if (window.scrollY > content.offsetTop) {
+    const percentage = ((window.scrollY - content.offsetTop) / content.scrollHeight * 100).toFixed(2)
+    scrollIndicator.style.transform = `scaleX(${(percentage / 100)})`
+  } else {
+    scrollIndicator.style.transform = "scaleX(0)"
+  }
+}
 
 // toggle nav links
 toggleLinksMenu.addEventListener("click", toggleNav);
