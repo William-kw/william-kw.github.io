71d4910654e39653182b3f1b9b75ae0fd1809b68
William-kw <williamkenfack4@gmail.com>
2024-03-14 16:35:38 +0100

    le 14 mars v2

--- php/send.php
+++ php/send.php
@@ -1,10 +1,15 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 $nom = htmlspecialchars($_POST["nom"]);
 $email = htmlspecialchars($_POST["email"]);
 $tel = htmlspecialchars($_POST["tel"]);
 $message = htmlspecialchars($_POST["message"]);
 
-$destinataire = "williamkenfack4@gmail.com";
 $sujet = "Nouveau mail de $nom";
 
 $corpsMail = "Nom: $nom\n";
@@ -12,32 +17,27 @@ $corpsMail .= "Email: $email\n";
 $corpsMail .= "Téléphone: $tel\n\n";
 $corpsMail .= "Message:\n$message";
 
-$headers = "De: $email\r\n";
+$mail = new PHPMailer(true);
 
-$send = mail($destinataire, $sujet, $corpsMail, $headers);
-if ($send) {
-    echo "succes";
-}
+try {
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+    $mail->isSMTP();                                         
+    $mail->Host       = 'smtp.gmail.com'; 
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'williamkenfack4@gmail.com';
+    $mail->Password   = 'dkniadggonyofmlr';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
-use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.hostinger.fr';
-$mail->Port = 587;
-$mail->SMTPAuth = true;
-$mail->Username = 'test@hostinger-tutorials.fr';
-$mail->Password = 'VOTRE MOT DE PASSE ICI';
-$mail->setFrom('test@hostinger-tutorials.fr', 'Votre nom');
-$mail->addReplyTo('test@hostinger-tutorials.fr', 'Votre nom');
-$mail->addAddress('exemple@gmail.com', 'Nom du destinataire');
-$mail->Subject = 'Essai de PHPMailer';
-$mail->msgHTML(file_get_contents('message.html'), __DIR__);
-$mail->Body = 'Ceci est le contenu du message en texte clair';
-//$mail->addAttachment('test.txt');
-if ($mail->send()) {
-    echo "succes";
-} else {
-    echo 'Erreur de Mailer : ' . $mail->ErrorInfo;
+    $mail->setFrom($email, $nom);
+    $mail->addAddress('williamkenfack4@gmail.com', 'William-kw');
+
+    $mail->isHTML(true);
+    $mail->Subject = $sujet;
+    $mail->Body    = $corpsMail;
+
+    $mail->send();
+    echo 'succes';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
